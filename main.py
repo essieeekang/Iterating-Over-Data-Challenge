e40bc11878e6d8a7b3c8833b3e027090754644ac
Esther Kang <estherkang.j@gmail.com>
2025-03-13 11:59:47 -0700

    added function bodies to pass tests

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 def calculate_total_sales(sales_totals):
-    pass
+    total_sales = 0
+
+    for data in sales_totals.values():
+        total_sales += data["quantity"] * data["price"]
+
+    return total_sales
 
 def calculate_average_sales(sales_totals):
-    pass
+    if len(sales_totals) == 0:
+        return 0
+    
+    return calculate_total_sales(sales_totals) / len(sales_totals)
 
 def filter_to_better_than_average_sales(sales_totals):
-    pass
+    filtered = {}
+
+    if not sales_totals:
+        return filtered
+    
+    average_sales = calculate_average_sales(sales_totals)
+
+    for candy, data in sales_totals.items():
+        sales = data["quantity"] * data["price"]
+        if sales > average_sales:
+            filtered[candy] = data
+    
+    return filtered
     
 def ninety_nine_bottles(count, beverage):
-    pass
+    lines = []
+
+    while count > 0:
+        if count == 1:
+            lines.append(f'{count} bottle of {beverage} on the wall, {count} bottle of {beverage}')
+        else:
+            lines.append(f'{count} bottles of {beverage} on the wall, {count} bottles of {beverage}')
+        if count - 1 == 1:
+            lines.append(f'If one of those bottles should happen to fall, {count - 1} bottle of {beverage} on the wall')
+        else:
+            lines.append(f'If one of those bottles should happen to fall, {count - 1} bottles of {beverage} on the wall')
+
+        count -= 1
+
+    return lines
